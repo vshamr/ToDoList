6f11009c386abfaead891b5ef4dffdbb7a661c6f
vshamr <vero94@mail.ru>
2021-06-09 21:48:47 +0300

    all changes 16

---
+++ src/app/App.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from './App'
+import {ReduxStoreProviderDecorator} from '../stories/decorators/ReduxStoreProviderDecorator'
+
+export default {
+    title: 'App Stories',
+    component: App,
+    decorators: [ReduxStoreProviderDecorator]
+}
+
+export const AppBaseExample = (props: any) => {
+    return (<App demo={true} />)
+}
